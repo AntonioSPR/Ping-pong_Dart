96a28172eec9b9f0c052de911739ecc762ea574b
Antonio <antonio.prieto.riera@gmail.com>
2020-06-05 12:10:19 +0200

    The court is draw
    I draw the court and solve a "weird error" in the canvas size.

--- ping-pong.dart
+++ ping-pong.dart
@@ -1,5 +1,42 @@
 import 'dart:html';
+import 'dart:math';
+import 'dart:collection';
+import 'dart:async';
+
+const B_COLOR = "black";
+const F_COLOR = "#00CC00"; // Link (Zelda) green
+
+CanvasElement canvas = querySelector('#canvas')..focus();
+CanvasRenderingContext2D ctx = canvas.getContext('2d');
+Keyboard keyboard = new Keyboard();
 
 void main() {
+    // I go to use the #text element to display error messages
+    Element text = querySelector('#text');
+    // text.text = "canvas.width = ${canvas.width} canvas.height = ${canvas.height}";
 
+   clear();
+
+}
+
+void clear() {
+    ctx..fillStyle = B_COLOR
+       ..fillRect(0, 0, canvas.width, canvas.height);
+
+    int midCourt = canvas.width ~/ 2;
+    ctx..strokeStyle = F_COLOR
+       ..setLineDash([20, 20]) // [dash, gap]
+       ..lineDashOffset = 10
+       ..lineWidth = 10
+       ..beginPath()
+       ..moveTo(midCourt , 0)
+       ..lineTo(midCourt , canvas.height)
+       ..stroke();
+
+
+
+}
+
+class Keyboard {
+    // TODO
 }
